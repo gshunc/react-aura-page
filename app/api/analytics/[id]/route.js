9417176dac032cff384b223382cca8984f460573
George Harris <gsh@ad.unc.edu>
2024-01-31 16:36:43 -0500

    Still not able to add projection to API call

--- app/api/analytics/[id]/route.js
+++ app/api/analytics/[id]/route.js
@@ -5,6 +5,6 @@ import { NextResponse } from "next/server";
 export async function GET(request, { params }) {
   const { id } = params;
   await connectMongoDB();
-  const activity = await Personal.findOne({ _id: id });
-  return NextResponse.json({ activity });
+  const activity = await Personal.findOne({ _id: id }).project({ activity: 1 });
+  return NextResponse.json({ response: activity }, { status: 200 });
 }
--- app/page.js
+++ app/page.js
@@ -1,28 +1,51 @@
-import Image from "next/image";
-import Header from "./components/Header";
 import GraphBox from "./components/GraphBox";
 import LineChart from "./components/LineChart";
-export default function Home() {
+import Header from "./components/Header";
+
+const getProfileInfoById = async (id) => {
+  try {
+    let res = await fetch(`http://localhost:3000/api/analytics/?id=${id}`, {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Error fetching information from user.");
+    }
+    return res.json();
+  } catch (error) {
+    console.error("Error in getProfileInfoById:", error);
+    throw new Error(
+      "Error fetching information about user. Details: " + error.message
+    );
+  }
+};
+export default async function Home() {
+  const info = JSON.stringify(
+    await getProfileInfoById("655768647ec91b0002a9d8b2")
+  );
+  console.log(info);
   return (
-    <main className="flex min-h-screen flex-col">
-      <Header></Header>
-      <div className="flex flex-row justify-around">
-        <div className="flex flex-col">
-          <GraphBox
-            title={"Step Activity"}
-            isLarge={true}
-            content={<LineChart></LineChart>}
-          ></GraphBox>
-          <GraphBox title={"Total Activity Level"} isLarge={true}></GraphBox>
+    <>
+      <main className="flex min-h-screen flex-col">
+        <Header />
+        <div className="flex flex-row justify-around">
+          <div className="flex flex-col">
+            <GraphBox
+              title={"Step Activity"}
+              isLarge={true}
+              content={<LineChart></LineChart>}
+            ></GraphBox>
+            <GraphBox title={"Total Activity Level"} isLarge={true}></GraphBox>
+          </div>
+          <div className="flex flex-col">
+            <GraphBox title={"Total Steps"}></GraphBox>
+            <GraphBox title={"Standing Time"}></GraphBox>
+          </div>
+          <div className="flex flex-col">
+            <GraphBox title={"Activity Profile"}></GraphBox>
+          </div>
         </div>
-        <div className="flex flex-col">
-          <GraphBox title={"Total Steps"}></GraphBox>
-          <GraphBox title={"Standing Time"}></GraphBox>
-        </div>
-        <div className="flex flex-col">
-          <GraphBox title={"Activity Profile"}></GraphBox>
-        </div>
-      </div>
-    </main>
+        <div>{info}</div>
+      </main>
+    </>
   );
 }
