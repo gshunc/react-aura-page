cd00fb3ffef17e7fdddb1a862da8f6670904584e
George Harris <gsh@ad.unc.edu>
2024-02-28 00:21:41 -0500

    maybe? bad practice solution

--- utils/dataProcessing.js
+++ utils/dataProcessing.js
@@ -21,9 +21,9 @@ export const formatDate = (raw_date) => {
   //Helper function used in multiple graphing components.
   const str_date = String(raw_date);
   const formattedDate = new Date(str_date).toLocaleString("en-US", {
+    timezone: "UTC",
     hour: "2-digit",
     minute: "2-digit",
-    timeZoneName: "short",
     hour12: false,
     day: "2-digit",
   });
