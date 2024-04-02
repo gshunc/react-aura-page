387ab127a7133f8813c2b4e425bd80a4425957b1
George Harris <gsh@ad.unc.edu>
2024-04-01 23:07:51 -0400

    trying to salvage

--- utils/dataProcessing.js
+++ utils/dataProcessing.js
@@ -71,7 +71,7 @@ export const processUserData = async (info, date, timezone) => {
     });
     currentTime.setMilliseconds(currentTime.getMilliseconds() + interval);
   }
-  console.log(res);
+  console.log(info);
   return res;
 };
 
