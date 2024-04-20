c77fbefcd83107008ef01a803e3f3d4f81ec46f1
George Harris <gsh@ad.unc.edu>
2024-04-20 17:21:22 -0400

    added suspense

--- app/components/layoutComps/HeaderLinks.jsx
+++ app/components/layoutComps/HeaderLinks.jsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import NameLabel from "../home/NameLabel";
 import { useSearchParams, useRouter } from "next/navigation";
+import { Suspense } from "react";
 
 export default function HeaderLinks() {
   const searchParams = useSearchParams();
@@ -13,17 +14,19 @@ export default function HeaderLinks() {
   };
 
   return userid ? (
-    <div className="text-blue-900 font-medium flex flex-row text-lg space-x-5 mr-5">
-      <NameLabel userid={userid} />
-      <Link
-        href={`/pages/home?userid=${encodeURIComponent(userid)}`}
-        className="underline"
-      >
-        {"Home"}
-      </Link>
-      <button onClick={handleLogout} className="underline">
-        {"Log Out"}
-      </button>
-    </div>
+    <Suspense>
+      <div className="text-blue-900 font-medium flex flex-row text-lg space-x-5 mr-5">
+        <NameLabel userid={userid} />
+        <Link
+          href={`/pages/home?userid=${encodeURIComponent(userid)}`}
+          className="underline"
+        >
+          {"Home"}
+        </Link>
+        <button onClick={handleLogout} className="underline">
+          {"Log Out"}
+        </button>
+      </div>
+    </Suspense>
   ) : null;
 }
