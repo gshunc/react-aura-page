f21576b9011430202846651983b74cbaf0c13e70
George Harris <gsh@ad.unc.edu>
2024-04-21 19:28:12 -0400

    Trying to index mongodb and optimize query to make page faster and fix breaking error with large data days

--- app/api/analytics/[id]/[date]/[timezone]/route.js
+++ app/api/analytics/[id]/[date]/[timezone]/route.js
@@ -3,40 +3,40 @@ import Personal from "../../../../../../models/Personal";
 
 export async function GET(request, { params }) {
   await connectMongoDB();
-
   const { id, date, timezone } = params;
   const startOfDay = new Date(date);
   startOfDay.setHours(timezone, 0, 0);
   const endOfDay = new Date(new Date(date).setDate(startOfDay.getDate() + 1));
   endOfDay.setHours(0, 0, 0);
 
-  let activity = await Personal.aggregate([
+  const user = await Personal.aggregate([
     {
       $match: {
         userid: id,
       },
     },
     {
-      $project: {
-        activity: {
-          $filter: {
-            input: "$activity",
-            cond: {
-              $and: [
-                {
-                  $gte: ["$$this.time", new Date(startOfDay.toISOString())],
-                },
-                {
-                  $lt: ["$$this.time", new Date(endOfDay.toISOString())],
-                },
-              ],
-            },
-          },
+      $unwind: "$activity",
+    },
+    {
+      $match: {
+        "activity.time": {
+          $gte: startOfDay,
+          $lt: endOfDay,
         },
       },
     },
+    {
+      $project: {
+        _id: 0,
+        activity: 1,
+      },
+    },
   ]);
-  activity = activity.length > 0 ? activity[0].activity : [];
-  console.log(activity.length);
+
+  console.log(user.length);
+
+  const activity = user ? user.map((a) => a.activity) : [];
+
   return Response.json({ response: activity }, { status: 200 });
 }
